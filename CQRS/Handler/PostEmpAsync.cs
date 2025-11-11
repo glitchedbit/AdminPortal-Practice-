3080a2057663b59a1bc0a9fc4b608875b37d9287
glitchedbit <dmt.work2025@hotmail.com>
2025-11-11 15:31:32 +0500

    finalized with scalar and logging logic

--- CQRS/Handler/PostEmpAsync.cs
+++ CQRS/Handler/PostEmpAsync.cs
@@ -8,15 +8,19 @@ using EmployeeAdmnPortal.Models.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using EmployeeAdminPortal.CQRS.Infrastructure;
+using EmployeeAdmnPortal.Services;
 namespace EmployeeAdmnPortal.CQRS.Handler
 {
     public class PostEmpAsync : IRequestHandler<post_emp_command, Employee>
     {
         private readonly IGenericRepository<Employee> repo;
+        private readonly ILoggerServices _log;
 
-        public PostEmpAsync(IGenericRepository<Employee> _repo)
+        public PostEmpAsync(IGenericRepository<Employee> _repo,ILoggerServices log)
         {
             repo = _repo;
+            _log = log;
+        
         }
 
         public Task<Employee> Handle(post_emp_command request, CancellationToken cancellationToken)
